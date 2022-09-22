71564cc4948f693d397f52767307420d56a9f2c6
Rajat Gupta <dguptarajat@gmail.com>
2022-09-22 21:47:42 +0530

    feat - Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Navbar } from "./components/index";
 
 function App() {
-  return <div className="App">HAAHAHAH</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
