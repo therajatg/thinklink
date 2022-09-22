e6f2f1fcbfbae98839ce74ff96857e0b7cba66cc
Rajat Gupta <dguptarajat@gmail.com>
2022-09-22 23:23:25 +0530

    feat - sidebar

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,26 @@
+import style from "./sidebar.module.css";
+import { HiDesktopComputer } from "react-icons/hi";
+import { TbStack2 } from "react-icons/tb";
+import { MdPeopleAlt } from "react-icons/md";
+import { FiShoppingBag, FiFileText, FiSettings } from "react-icons/fi";
+
+export const Sidebar = () => {
+  const options = [
+    { type: "Dashboard", icon: <HiDesktopComputer /> },
+    { type: "Inventory", icon: <TbStack2 /> },
+    { type: "Orders", icon: <FiShoppingBag /> },
+    { type: "Customers", icon: <MdPeopleAlt /> },
+    { type: "Reports", icon: <FiFileText /> },
+    { type: "Settings", icon: <FiSettings /> },
+  ];
+  return (
+    <aside>
+      {options.map(({ type, icon }) => (
+        <div className={style.option} id={type}>
+          {icon}
+          {type}
+        </div>
+      ))}
+    </aside>
+  );
+};
