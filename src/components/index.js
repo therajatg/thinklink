ce10b9efe49b5f2b402d4c3c41f6dd6febb71cda
Rajat Gupta <dguptarajat@gmail.com>
2022-09-28 12:36:13 +0530

    feat - sales detail

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { Navbar } from "./navbar/Navbar";
 export { Sidebar } from "./sidebar/Sidebar";
 export { OrderDetails } from "./orderDetails/OrderDetails";
+export { LineGraph } from "./lineGraph/LineGraph";
+export { SalesDetail } from "./salesDetail/SalesDetail";
